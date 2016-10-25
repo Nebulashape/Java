9302349fe21041658372ee9cd74202655fc16292
Nebulashape <kasonculbertson8@gmail.com>
2016-10-25 08:33:32 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,20 @@
+class Asc {
+	public static void main (String a[]){
+		//variables 
+	int i;// integer 
+	char c;// charector c
+	//for loop
+	for (i = 32; i < 256; i++){
+		System.out.println(i);
+			c = (char)i;
+			System.out.print(i+"="+c+" ");
+			System.out.print(" ");System.out.print(c);System.out.print(" ");		
+			
+			
+			}
+							
+	}
+
+
+
+}
